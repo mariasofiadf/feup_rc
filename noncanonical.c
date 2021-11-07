e6fdc91b30bd4d0af59b562f48ad9a3bafa65780
mariasofiadf <up201904675@fe.up.pt>
2021-11-07 13:43:23 +0000

    Fixed makefile

--- noncanonical.c
+++ noncanonical.c
@@ -6,10 +6,10 @@
 #include <termios.h>
 #include <stdio.h>
 
+#include "header.h"
+
 #define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
+
 
 volatile int STOP=FALSE;
 
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -6,11 +6,10 @@
 #include <termios.h>
 #include <stdio.h>
 
+#include "header.h"
+
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
 
 volatile int STOP=FALSE;
 
